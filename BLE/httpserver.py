486dfc32b448635f174fc7e3e04fa1b058a1ef3f
Edwios <edwintam@iostation.com>
2019-01-18 19:10:43 +0800

    MQTT now works in the HTTP server

--- BLE/httpserver.py
+++ BLE/httpserver.py
@@ -1,14 +1,21 @@
 from aiohttp import web
 from subprocess import call
+import paho.mqtt.client as mqtt
+import os.path
 import time
 
 HOST_NAME = 'localhost'
 PORT_NUMBER = 80
 CMDEXE = '/home/edwintam/BLE/lds.py'
 
+_mqtthub = "127.0.0.1"
+connected = False
+
 async def HttpHandler(request):
 		_lasthttpcmd = request.app['_lasthttpcmd']
+		mqttclient = request.app['mqttclient']
 		_httpcmd = None
+
 		cmd = request.match_info.get('cmd', "off")
 		if cmd == "on":
 			print("Received on from HTTP")
@@ -20,12 +27,39 @@ async def HttpHandler(request):
 			request.app['_lasthttpcmd'] = _httpcmd
 			print("Recevied %s from HTTP" % _httpcmd)
 			if (_httpcmd == "on"):
-				call([CMDEXE, "-d", "1", "on"])
+				mqttclient.publish("sensornet/command", "on")
+#				call([CMDEXE, "-d", "1", "on"])
 			if (_httpcmd == "off"):
-				call([CMDEXE, "-d", "1", "off"])
+				mqttclient.publish("sensornet/command", "off")
+#				call([CMDEXE, "-d", "1", "off"])
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+	global connected
+	connected = True
+	print("Connected with result code "+str(rc))
+	client.subscribe([("sensornet/env/balcony/brightness", 0), ("sensornet/all", 0), ("sensornet/command", 0)])
+
+def on_disconnect(client, userdata, rc):
+	global connected
+	connected = False
+	if rc != 0:
+		print("Unexpected disconnection.")
+
+def on_message(client, userdata, msg):
+	print(msg.topic+" "+str(msg.payload))
+
+print("Starting mqtt")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.connect_async(_mqtthub, 1883, 60)
+client.loop_start() #start loop to process received messages
+print("mqtt started")
 app = web.Application()
 app['_lasthttpcmd'] = ""
+app['mqttclient'] = client
 app.add_routes([web.get('/cmd/{cmd}', HttpHandler)])
 print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
 web.run_app(app, port=PORT_NUMBER)
--- BLE/smartline_demo.sh
+++ BLE/smartline_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/edwintam/dotIoT/BLE
-/usr/bin/python3 -u leGateway.py >> leGateway_log.txt &
-/usr/bin/python3 -u httpserver.py &
-#/usr/bin/python3 -u lds.py >> lds_log.txt &
+/usr/bin/python3 -u httpserver.py >> httpserver_log.txt &
+/usr/bin/python3 -u lds.py -d 1 wait >> lds_log.txt &
+#/usr/bin/python3 -u leGateway.py >> leGateway.txt &
 
