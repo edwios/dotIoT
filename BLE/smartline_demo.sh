9f1ec805627a53a90a97487c1f6c19f63175a786
edwios <edwintam@iostation.com>
2019-06-08 10:56:21 +0800

    Using correct -a option for automatic mode

--- BLE/smartline_demo.sh
+++ BLE/smartline_demo.sh
@@ -4,7 +4,7 @@ cd ~/dotIoT/BLE
 /usr/bin/python3 -u httpserver.py >> httpserver_log.txt &
 # Next line you will need to change the device number [-d] to the one you want to control.
 # To lookup the device number, execute `lds.py -c on`
-/usr/bin/python3 -u lds.py -d 10 wait >> lds_log.txt &
+/usr/bin/python3 -u lds.py -a -N 8P9T2NFG -P 302771 wait >> lds_log.txt &
 #
 # Uncomment the following is a BLE lux sensor is used
 # /usr/bin/python3 -u leGateway.py -P 600 >> leGateway_log.txt &
