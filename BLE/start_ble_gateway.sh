b2fce7975d0f089a4d431482c76f04ddfa64d034
edwios <edwintam@iostation.com>
2020-07-06 11:14:03 +0800

    Hard restart Bluetooth on RPi

--- BLE/start_ble_gateway.sh
+++ BLE/start_ble_gateway.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+FLAGG=$1
 while true; do
-python -u smartline2.py -s house --mqtthost 10.0.1.250 wait
+python -u smartline.py -R -s house --mqtthost 10.0.1.250 ${FLAGG} wait
 echo "Process dead, relaunching in 5s"
-sleep 5
+systemctl restart bluetooth
+sleep 1
 done
