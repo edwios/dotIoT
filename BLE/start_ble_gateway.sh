6c882ad88a7a2526ec4805473d4698d32d6784c3
Edwios <edwintam@iostation.com>
2020-10-13 08:24:04 +0200

    Proper termination via shell

--- BLE/start_ble_gateway.sh
+++ BLE/start_ble_gateway.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 FLAGG=$1
 while true; do
-python -u smartline.py -R -s house --mqtthost 10.0.1.250 ${FLAGG} wait
-echo "Process dead, relaunching in 5s"
+python3 -u smartline.py -R -s house --mqtthost 10.0.1.250 ${FLAGG} wait
+if [ $? -eq 0 ]; then
+    echo "Process terminated normally"
+    exit 0
+else
+    echo "Process dead, relaunching in 5s"
+fi
 systemctl restart bluetooth
 sleep 1
 done
