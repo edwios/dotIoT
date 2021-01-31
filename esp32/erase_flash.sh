5513e186cb8b3d4b7c2dc214398e685459d21b73
edwios <edwintam@iostation.com>
2021-01-31 21:07:38 +0100

    Defined PORT

--- esp32/erase_flash.sh
+++ esp32/erase_flash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PORT="/dev/tty.SLAB_USBtoUART"
 python3 pgm.py
-esptool.py --chip esp32 --baud 1500000 --port /dev/ttyS0 erase_flash
+esptool.py --chip esp32 --baud 1500000 --port ${PORT} erase_flash
 
