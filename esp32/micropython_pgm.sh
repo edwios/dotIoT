d52562c48ef041888f403f6e78f02139395d01c2
Edwin Tam <edwin.tam@telldus.se>
2021-11-29 15:26:38 +0100

    µPython 1.17

--- esp32/micropython_pgm.sh
+++ esp32/micropython_pgm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-MPBIN=esp32-idf3-20200902-v1.13.bin
+MPBIN=esp32spiram-20210902-v1.17.bin
 ERASEBAUD=1500000
 PGMBAUD=5000000
-PORT=/dev/tty.usbserial-AB0JJJ6L
+#PORT=/dev/tty.usbserial-AB0JJJ6L
 #PORT=/dev/ttyS0
+PORT=/dev/tty.SLAB_USBtoUART
 python3 pgm.py
-# esptool.py --chip esp32 --baud ${ERASEBAUD} --port /dev/ttyS0 erase_flash
+esptool.py --chip esp32 --baud ${ERASEBAUD} --port ${PORT} erase_flash
 #esptool.py --chip esp32 --port /dev/ttyS0 --baud ${PGMBAUD} write_flash -z 0x1000 ${MPBIN}
 esptool.py --chip esp32 --port ${PORT} write_flash -z 0x1000 ${MPBIN}
 
