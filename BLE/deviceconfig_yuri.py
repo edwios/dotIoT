627d2a3e6ed1d586f734d5089c82b07bc2ed1d51
edwios <edwintam@iostation.com>
2022-04-04 19:14:57 +0200

    UCT devices

--- BLE/deviceconfig_yuri.py
+++ BLE/deviceconfig_yuri.py
@@ -1,3 +1,7 @@
 devices = {}
 devices['E7:7C:12:1F:73:24'] = 'outdoor'
 devices['D7:6F:BE:6A:88:7D'] = 'studyroom'
+bl01_devices = {}
+bl01_devices['A4:C1:38:18:C5:A2'] = 'uct receiver'
+bl01_devices['A4:C1:38:88:88:99'] = 'uct livingroom'
+bl01_devices['A4:C1:38:29:DF:A4'] = 'uct outdoor'
