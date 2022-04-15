6fdad699e870c7203eba8a10bf709b7a57fda293
edwios <edwintam@iostation.com>
2022-04-15 09:08:06 +0200

    New TH sensors

--- BLE/deviceconfig_tammytam.py
+++ BLE/deviceconfig_tammytam.py
@@ -2,3 +2,6 @@ devices = {}
 devices['CF:9E:FC:96:A3:5D'] = 'hallway'
 devices['CD:A0:4B:46:E5:8A'] = 'masterbedroom'
 devices['F2:D6:31:89:17:43'] = 'livingroom'
+bl01_devices = {}
+bl01_devices['A4:C1:38:18:C5:A2'] = 'uct receiver'
+bl01_devices['A4:C1:38:88:88:99'] = 'uct livingroom'
